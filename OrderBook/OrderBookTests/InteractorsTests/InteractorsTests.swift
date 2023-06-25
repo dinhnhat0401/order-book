ba6bf18ada589b4edf4397be547bd01cf691c697
dinhnhat0401 <nhatphutho@gmail.com>
2023-06-25 11:03:10 +0900

    more test

--- OrderBook/OrderBookTests/InteractorsTests/InteractorsTests.swift
+++ OrderBook/OrderBookTests/InteractorsTests/InteractorsTests.swift
@@ -5,13 +5,28 @@
 //  Created by Đinh Văn Nhật on 2023/06/19.
 //
 
+import Combine
 import XCTest
 @testable import Interactors
 @testable import Repositories
 @testable import Services
+import Entities
 
 final class InteractorsTests: XCTestCase {
-    func testCanGetDataViaRepository() {
-        // TODO: test this
+    func testCanGetDataViaRepository() async {
+        var cancellable = Set<AnyCancellable>()
+        let expectation = XCTestExpectation(description: "Fetch data via websocket")
+        let marketDataInteractor = MarketDataInteractor()
+        marketDataInteractor.connect()
+        try! marketDataInteractor.subscribe(topics: ["trade:XBTUSD", "orderBookL2:XBTUSD"])
+        marketDataInteractor.streamData()
+        marketDataInteractor.orderBookValueSubject
+            .dropFirst()
+            .sink { data in
+                print(data)
+                expectation.fulfill()
+            }
+            .store(in: &cancellable)
+        await fulfillment(of: [expectation], timeout: 10)
     }
 }
